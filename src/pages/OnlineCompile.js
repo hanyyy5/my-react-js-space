05726e14cb64b00d704667c25f8db8e78b65178c
yuhan <hanyustu@163.com>
2019-04-24 17:30:04 +0800

    new

--- src/pages/OnlineCompile.js
+++ src/pages/OnlineCompile.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
-
+import '../css/online-compile.scss';
 class OnlineCompile extends Component {
     render() {
+        const compileData = {
+            author: "yuhan",
+            title: "Untitled"
+        }
         return (
-            <div className="login">
-                <h1>在线编译</h1>
+            <div className="online-compile">
+                <header>
+                    <div className="item-title-area">
+                        <div className="item-title-text">
+                            <h1 className="item-title" id="item-title">
+                                <span className="item-title-link">{compileData.title}</span>
+                            </h1>
+                            <div className="by">创建者<a class="item-owner-link" href="/hanyyy5">{compileData.author}</a>
+                            </div>
+                        </div>
+                    </div>
+                </header>
             </div>
+            
         )
     }
 }
