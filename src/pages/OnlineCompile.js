b810a00a10845ef2727fd0d5046a7db9f9d3a830
yuhan <hanyustu@163.com>
2019-04-28 17:49:34 +0800

    edit page

--- src/pages/OnlineCompile.js
+++ src/pages/OnlineCompile.js
@@ -14,39 +14,58 @@ class OnlineCompile extends Component {
                             '</head>' + 
                             '<style>'
         this.fileContent2 = '</style><body>'
-        this.fileContent3 = '</body></html>'
+        this.fileContent3 = '</body><script>'
+        this.fileContent4 = '</script></html>'
         this.state = {
             inputContent1: '<p class="test">hello</p>',
             inputContent2: '.test {color: red}',
-            iframeSrc: ""
+            inputContent3: '',
+            iframeSrc: "",
+            timeAgo: "1分钟前"
         }
         this.preview = this.preview.bind(this)
     }
     preview = (event) => {
+        console.log(event.target.name)
         this.setState({inputContent1: event.target.value}, ()=> {
-             let ifr = document.getElementById("result")
              let blob = new Blob([this.fileContent1, this.state.inputContent2,
                                     this.fileContent2, this.state.inputContent1,   
-                                    this.fileContent3 ], {
+                                    this.fileContent3, this.state.inputContent3,
+                                    this.fileContent4], {
                     'type': 'text/html'
              }); 
-             this.setState({iframeSrc:  URL.createObjectURL(blob) })
+             this.setState({iframeSrc: URL.createObjectURL(blob)})
         })
     }
-
     render() {
         const compileData = {
             author: "yuhan",
             title: "Untitled"
         }
         const editIcon = require('../images/pen.png')
-        const headerDivIcon = {
-            save: require('../images/save.png'),
-            setting: require('../images/setting.png'),
-            changeView: require('../images/eye.png')
-        }
+        const headerDivIcon =[
+            {
+                className: "save",
+                imgIcon: require('../images/save.png'),
+                title: "保存文件"
+            },
+            {
+                className: "setting",
+                imgIcon: require('../images/setting.png'),
+                title: "设置"
+            },
+            {
+                className: "changeView",
+                imgIcon: require('../images/eye.png'),
+                title: "改变视图"
+            }
+           
+        ]
         const collectIcon = require('../images/pin.png')
         const bottomArrowIcon = require('../images/bottom_arrow.png')
+        const footerLeft = ["控制台", "资源", "评论", "快捷方式"]
+        const footerRight = ["删除", "嵌入", "导出", "分享"]
+        const openNewIcon = require('../images/open-new.png')
         return (
             <div className="online-compile">
                 <header>
@@ -62,9 +81,10 @@ class OnlineCompile extends Component {
                     </div>
                     <div className="navigation-wrap">
                          {
-                            Object.keys(headerDivIcon).map(key => {
-                                return <span className={`nav-button ${key}`} key={key}>
-                                             <img src={headerDivIcon[key]} alt=""/>
+                            headerDivIcon.map((item,index)=>{
+                                return <span className={`nav-button ${item.className}`} key={index}
+                                             title={item.title}>
+                                                <img src={item.imgIcon} alt=""/>
                                        </span>
                             })
                          }
@@ -75,15 +95,63 @@ class OnlineCompile extends Component {
                     </div>
                 </header>
                 <div className="edit-wrapper">
-                    <Form>
-                        <TextArea placeholder='编写html' style={{ minHeight: 200 }} 
+                    
+                    <Form className="edit-box html-box">
+                        <div className="edit-resizer html-resizer"></div>
+                        <div className="edit-setting html-setting">
+                             <span className="setting">
+                                   <img src="" alt=""/>
+                             </span>
+                        </div>
+                        <TextArea className="edit-textarea html-resizer" placeholder='编写html'
+                                  name="test1"
                                   value={this.state.inputContent1}
                                   onChange={this.preview}/>
                     </Form>
+                    <Form className="edit-box css-box">
+                        <div className="edit-resizer css-resizer"></div>
+                        <div className="edit-setting css-setting"></div>
+                        <TextArea className="edit-textarea" placeholder='编写css'
+                                  name="test2"
+                                  value={this.state.inputContent2}
+                                  onChange={this.preview}/>
+                    </Form>
+                    <Form className="edit-box js-box">
+                        <div className="edit-resizer js-resizer"></div>
+                        <div className="edit-setting js-setting"></div>
+                        <TextArea className="edit-textarea" placeholder='编写js'
+                                  name="test1" 
+                                  value={this.state.inputContent3}
+                                  onChange={this.preview}/>
+                    </Form>
+                </div>
+                <div className="resizer">
                 </div>
                 <div className="output-container">
-                    <iframe src={this.state.iframeSrc} frameBorder="0" id="result"></iframe>
+                    <iframe src={this.state.iframeSrc} frameBorder="0" id="result" width="100%" height="100%"></iframe>
                 </div>
+                <footer className="compile-page-footer">
+                     <div className="footer-left">
+                          {
+                               footerLeft.map((item, index)=>{
+                                   return <span className="footer-btn" key={index}>{item}</span>
+                               })
+                          }
+                     </div>
+                     <div className="footer-right">
+                          {
+                              footerRight.map((item, index)=>{
+                                return <span className="footer-btn" key={index}>{item}</span>
+                              })
+                          }
+                          <span className="usual">
+                              最后一次保存为<span className="time-ago">{this.state.timeAgo}</span>
+                          </span>
+                          <span className="open-new">
+                              <img src={openNewIcon} alt=""/>
+                          </span>
+                     </div>
+                </footer>
             </div>
             
         )
